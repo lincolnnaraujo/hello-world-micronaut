936110784485fb70fb43c61d115a4dfa864314bf
Lincoln Araujo <lincoln.n.araujo@gmail.com>
2021-07-27 16:17:30 -0300

    Testando configurações de yml

--- src/main/java/br/com/pocs/controller/HelloController.java
+++ src/main/java/br/com/pocs/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-@Controller("/hello")
+@Controller("${hello.controller.path}")
 public class HelloController implements HelloControllerRoutes {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
